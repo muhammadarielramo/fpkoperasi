5d0c380b2771fe028dc5b9715c36926cfb467197
Muhammad Ariel Ramo <if23.muhammadramo@mhs.ubpkarawang.ac.id>
2025-06-05 16:26:54 +0700

    test

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.kokita.app',
   appName: 'Kokita',
   webDir: 'www',
   plugins: {
